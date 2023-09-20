a2cb7ab3b3ae2b1d80d87807843cf3b17b78accd
saadboufarhi <saad.boufarhi@gmail.com>
2023-09-21 00:37:56 +0100

    first commit

--- layouts/admin_menu.php
+++ layouts/admin_menu.php
@@ -2,10 +2,10 @@
  <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
 <!-- Sidebar - Brand -->
-<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
     <div class="sidebar-brand-icon rotate-n-15">
-    <i class="fab fa-teamspeak"></i> </div>
-    <div class="sidebar-brand-text mx-3">INVENTORY </div>
+    <i class="fas fa-dolly-flatbed"></i></div>
+    <div class="sidebar-brand-text mx-3">INVENTORIES </div>
 </a>
 
 <!-- Divider -->
